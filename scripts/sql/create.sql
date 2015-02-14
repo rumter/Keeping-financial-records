612afaaadeabb7945c969cff819a7b77cb10196c
Ilya Mitin <rumterg@gmail.com>
2015-02-14 14:28:05 +0300

    fix comment

--- scripts/sql/create.sql
+++ scripts/sql/create.sql
@@ -1,12 +1,12 @@
 ﻿-- DROP DATABASE koala;
 
-/*CREATE DATABASE koala
+CREATE DATABASE koala
   WITH OWNER = postgres
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
        LC_COLLATE = 'Russian_Russia.1251'
        LC_CTYPE = 'Russian_Russia.1251'
-       CONNECTION LIMIT = -1;*/
+       CONNECTION LIMIT = -1;
 
 -- DROP TABLE exp_category;
 
