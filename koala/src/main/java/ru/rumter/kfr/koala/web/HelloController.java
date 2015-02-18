59145152cd27e8c002073aaa9e925a9a523e6008
Ilya Mitin <mitin.ilya@int.spb.ru>
2015-02-18 12:42:25 +0300

    add ExtJS framework
    add index.html

--- koala/src/main/java/ru/rumter/kfr/koala/web/HelloController.java
+++ koala/src/main/java/ru/rumter/kfr/koala/web/HelloController.java
@@ -1,27 +1,31 @@
 package ru.rumter.kfr.koala.web;
 
-import java.util.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping("")
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("/hello")
 @Controller
 public class HelloController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(Model model) {
-		model.addAttribute("name", "world");
-		return "index";
-	}
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model) {
+        model.addAttribute("name", "world");
+        return "hello";
+    }
 
-	@RequestMapping(value = "/json", produces="application/json", method = RequestMethod.GET)
-	@ResponseBody
-	public Map<String, Object> json() {
-		Map<String, Object> m = new HashMap<String, Object>();
-		m.put("success", true);
-		m.put("data", "Hello!");
-		return m;
-	}
+    @RequestMapping(value = "/json", produces = "application/json", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> json() {
+        Map<String, Object> m = new HashMap<String, Object>();
+        m.put("success", true);
+        m.put("data", "Hello!");
+        return m;
+    }
 
 }
